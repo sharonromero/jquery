086482a81ee94178ab79c3130049992c31d8700d
sharonromero <sh-aron@hotmail.com>
2019-04-03 16:53:27 -0600

    Updated index.html, todos.css, todos.js
    Added div with h1 and ul. Added some styling to container. Added jQuery so todo items change to gray, get crossed out, and fade out & delete when completed.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,12 @@
-alert("Connected...");
+// Check off specific todos by clicking
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
